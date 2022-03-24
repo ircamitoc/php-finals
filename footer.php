00a817298c3916296d63e413d9b497812010d238
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2022-03-24 15:29:40 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,21 @@
-<footer>
-    <h2>Page Footer</h2>
-</footer>
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/017ae98281.js" crossorigin="anonymous"></script>
+
+<section class="footer">
+    <div class="social">
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-facebook"></i></a>
+    </div>
+
+    <ul class="list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Terms</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+    </ul>
+    <p class="copyright">
+        Ice Skating @ 2022
+    </p>
+</section>
