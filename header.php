67dfe14149cb4d971beffb9029d0abc215cf7d34
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2022-03-24 15:10:15 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
 <link rel="stylesheet" href="style.css">
-
+<script src="https://kit.fontawesome.com/017ae98281.js" crossorigin="anonymous"></script>
 <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
     <label class="logo">DesignX</label>
     <ul>
-        <li><a href="#">Home</a></li>
+        <li><a class="active" href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Schedule</a>
         <li><a href="#">Reservation</a></li>
