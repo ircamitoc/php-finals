ce6598ad9c4956f4c6cd79c2990ac3d5df791448
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2022-03-24 14:54:42 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,20 +1,12 @@
-<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 <link rel="stylesheet" href="style.css">
 
-<div class="sidebar">
-    <h2>Skating Rink Reservation System</h2>
+<nav>
+    <label class="logo">DesignX</label>
     <ul>
-        <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
-        <li><a href="#"><i class="fas fa-user"></i>Profile</a></li>
-        <li><a href="#"><i class="fas fa-address-card"></i>About</a></li>
-        <li><a href="#"><i class="fas fa-project-diagram"></i>portfolio</a></li>
-        <li><a href="#"><i class="fas fa-blog"></i>Blogs</a></li>
-        <li><a href="#"><i class="fas fa-address-book"></i>Contact</a></li>
-        <li><a href="#"><i class="fas fa-map-pin"></i>Map</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Schedule</a>
+        <li><a href="#">Reservation</a></li>
+        <li><a href="#">Login</a></li>
     </ul>
-    <div class="social_media">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-    </div>
-</div>
+</nav>
