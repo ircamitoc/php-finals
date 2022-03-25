1159c7bb16aa8356effa161780b2262825b20f01
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2022-03-25 18:46:26 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,30 @@
-<link rel="stylesheet" href="style.css">
-<script src="https://kit.fontawesome.com/017ae98281.js" crossorigin="anonymous"></script>
-<nav>
-    <input type="checkbox" id="check">
-    <label for="check" class="checkbtn">
-        <i class="fas fa-bars"></i>
-    </label>
-    <label class="logo">Ice Skating <i class="fa-solid fa-person-skating"></i></label>
-    <ul>
-        <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Schedule</a>
-        <li><a href="reg_form.php">Reservation</a></li>
-        <li><a href="#">Login</a></li>
-    </ul>
+<nav class="navbar navbar-expand-xxl navbar-dark bg-black fixed-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand mb-0 h1"><i class="fa-solid fa-person-skating">&nbsp;&nbsp;I C
+                E&nbsp;&nbsp;S K A T I N G</i>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarExample01">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+        </div>
+        <form class="d-flex">
+            <!-- <input type="text" class="form-control me-2"> -->
+            <button type="submit" class="btn btn-outline-light">Sign Up</button>
+        </form>
+    </div>
 </nav>
