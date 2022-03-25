07d1ef1499ff285d6fac7636933a1d924133403d
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2022-03-25 10:38:58 +0800

    Update reg_form.php

--- reg_form.php
+++ reg_form.php
@@ -10,6 +10,7 @@
     <title>Reservation</title>
 </head>
 <?php include('header.php'); ?>
+
 <body>
     <div class="container">
         <div class="title">Registration</div>
@@ -17,49 +18,55 @@
             <div class="user-details">
                 <!-- FIRST NAME -->
                 <div class="input-box">
-                    <span class="details">First Name</div>
-                    <input type="text" placeholder="Enter First Name" required>
+                    <span class="details">First Name
                 </div>
-                <!-- LAST NAME -->
-                <div class="input-box">
-                    <span class="details">Last Name</div>
-                    <input type="text" placeholder="Enter Last Name" required>
-                </div>
-                <!-- EMAIL ADDRESS -->
-                <div class="input-box">
-                    <span class="details">Email</div>
-                    <input type="email" placeholder="Email Address" required>
-                </div>
-                <!-- DATE -->
-                <div class="input-box">
-                    <span class="details">Date</div>
-                    <input type="date" required>
-                </div>
-                <!-- TIME -->
-                <div class="input-box">
-                    <span class="details">Time</div>
-                    <input type="time" required>
-                </div>
+                <input type="text" placeholder="Enter First Name" required>
             </div>
-            <!-- POLAR BEAR -->
-            <div class="pb-details">
-                <span class="pb-rent-title">Polar Bear</span>
-                <div class="category">
-                    <label for="">
-                        <span class="dot-one"></span>
-                        <span class="polar-bear">Yes</span>
-                    </label>
-                    <label for="">
-                        <span class="dot-one"></span>
-                        <span class="polar-bear">No</span>
-                    </label>
-                </div>
+            <!-- LAST NAME -->
+            <div class="input-box">
+                <span class="details">Last Name
             </div>
-            <div class="button">
-                <input type="submit" value="Book Now">
-            </div>
-        </form>
+            <input type="text" placeholder="Enter Last Name" required>
+    </div>
+    <!-- EMAIL ADDRESS -->
+    <div class="input-box">
+        <span class="details">Email
+    </div>
+    <input type="email" placeholder="Email Address" required>
+    </div>
+    <!-- DATE -->
+    <div class="input-box">
+        <span class="details">Date
+    </div>
+    <input type="date" required>
+    </div>
+    <!-- TIME -->
+    <div class="input-box">
+        <span class="details">Time
+    </div>
+    <input type="time" required>
+    </div>
+    </div>
+    <!-- POLAR BEAR -->
+    <div class="pb-details">
+        <span class="pb-rent-title">Polar Bear</span>
+        <div class="category">
+            <label for="">
+                <span class="dot-one"></span>
+                <span class="polar-bear">Yes</span>
+            </label>
+            <label for="">
+                <span class="dot-one"></span>
+                <span class="polar-bear">No</span>
+            </label>
+        </div>
+    </div>
+    <div class="button">
+        <input type="submit" value="Book Now">
+    </div>
+    </form>
     </div>
 </body>
 <?php include('footer.php'); ?>
+
 </html>
