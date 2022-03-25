64398cf6d24fa7eced0f7e9fffb8499a0fbff2c3
Isaiah Rain R. Camitoc <81004648+ircamitoc@users.noreply.github.com>
2022-03-25 14:14:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--     <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script> -->
+    <link rel="stylesheet" href="style.css">
     <title>Skating Rink Reservation System</title>
 </head>
 <body>
     <?php include('header.php'); ?>
-    <div class="container">
-        <h1>HOME</h1>
-    </div>
+
     <?php include('footer.php'); ?>
 </body>
 
